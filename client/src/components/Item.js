e18e8a3eadf9121fc2d523239e6fc191e2d5b513
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-24 16:23:52 -0700

    delete function

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -5,17 +5,19 @@ const styles = {
   complete: { color: 'grey', textDecoration: 'line-through' }
 }
 
-const Item = ({name, id, complete, todoClick}) => (
-  <li
-    style={ complete ? {...styles.item, ...styles.complete } : styles.item }
-    onClick={() => todoClick(id)}
-    className="list-items"
-  >
-    {name}
-    <div className="delete">
-      <h4>X</h4> 
+const Item = ({name, id, complete, todoClick, deleteItem}) => (
+  <div style={{width: '500px'}}>
+    <li
+      style={ complete ? {...styles.item, ...styles.complete } : styles.item }
+      onClick={() => todoClick(id)}
+      className="list-items"
+    >
+      {name} 
+    <div>
+      <a onClick={deleteItem}class="waves-effect waves-light btn">Delete</a>
     </div>
-  </li>
+    </li>
+  </div>
 )
 
 export default Item
